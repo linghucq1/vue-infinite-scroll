ba56c1eaef241e7f4a5231495ab77c8aee4d9413
link.linghu <link.linghu@huolala.cn>
2019-12-04 11:00:28 +0800

    demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,128 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="scroll">
+      <InfiniteScroll :loadMore="getList" unique-key="name" :page-size="pageSize">
+        <template v-slot="{ item, index }">
+          <div class="user-item">
+            <img class="avatar" :src="item.avatar" alt="">
+            <div class="user">
+              <div class="username">{{item.name}}</div>
+              <div class="age">年龄 {{item.age}}</div>
+              <template v-if="item.gender === 1">
+                <div class="age">yo~~~</div>
+                <div class="age">yo~~~</div>
+                <div class="age">yo~~~</div>
+              </template>
+            </div>
+          </div>
+        </template>
+      </InfiniteScroll>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import InfiniteScroll from './components/infinite-scroll'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      InfiniteScroll
+    },
+    data() {
+      return {
+        list: [],
+        page: 0,
+        pageSize: 20,
+        lastItem: '',
+        intersectionObserver: null,
+        showLoading: ''
+      }
+    },
+    created() {
+    },
+    mounted() {
+
+    },
+    methods: {
+      async getList() {
+        this.page++;
+
+        const list = await this.requestList({ page: this.page, pageSize: this.pageSize })
+
+        this.list.push(...list)
+        return Promise.resolve(list)
+      },
+      requestList({ page, pageSize }) {
+        return new Promise((resolve) => {
+          const start = (page - 1) * pageSize
+          const end = page * 20
+          const list = []
+
+          for (let i = start; i < end; i++) {
+            list.push({
+              name: '路人' + i,
+              age: Math.floor(Math.random() * 100),
+              gender: Math.floor(Math.random() * 2),
+              avatar: 'https://user-gold-cdn.xitu.io/2019/5/15/16ab9a1cccad1ef3?imageView2/1/w/180/h/180/q/85/format/webp/interlace/1'
+            })
+          }
+
+          setTimeout(() => {
+            return resolve(list)
+          }, 1000)
+        })
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .scroll {
+    width: 400px;
+    height: 600px;
+    overflow: scroll;
+    border: 1px solid #e5e6e7;
+    border-radius: 4px;
+  }
+
+  .user-item {
+    display: flex;
+    align-items: center;
+    padding: 16px;
+    border-bottom: 1px solid #e5e6e7;
+  }
+
+  .user {
+    padding-left: 16px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .username {
+    font-size: 18px;
+  }
+
+  .age {
+    font-size: 14px;
+    color: #999;
+  }
+
+  .avatar {
+    width: 60px;
+    height: 60px;
+  }
 </style>
---
+++ src/components/infinite-scroll.vue
@@ -0,0 +1,195 @@
+<template>
+  <div class="infinite-scroll" :style="{ paddingTop: paddingTop + 'px', paddingBottom: paddingBottom + 'px' }">
+    <div class="wrap" v-for="panel in scrollList" :key="panel.page" :class="panel.ref" :ref="panel.ref">
+      <div class="list">
+        <div v-for="(item, index) in panel.list" :key="item[uniqueKey] || index" :ref="item.ref" :id="item.id">
+          <slot :item="item" :index="index"></slot>
+        </div>
+      </div>
+    </div>
+    <div class="loading" v-show="loading">
+      <slot name="loading">加载中...</slot>
+    </div>
+    <div class="ended" v-if="ended">
+      <slot name="ended">没有更多了</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  const FIRST_ITEM_ID = '__infinite-scroll-first-item__';
+  const LAST_ITEM_ID = '__infinite-scroll-last-item__';
+  export default {
+    name: "infinite-scroll",
+    props: {
+      uniqueKey: String,
+      loadMore: {
+        required: true,
+        type: Function,
+        default: () => Promise.resolve()
+      },
+      pageSize: {
+        type: Number,
+        required: true
+      },
+    },
+    data() {
+      return {
+        list: [],
+        loading: false,
+        ended: false,
+        scrollList: [],
+        intersectionObserver: null,
+        firstItem: '',
+        lastItem: '',
+        page: 0,
+        paddingTop: 0,
+        paddingBottom: 0
+      }
+    },
+    created() {
+      this._loadMore()
+
+      this.intersectionObserver = new IntersectionObserver(entries => {
+        for (let entry of entries) {
+          if (!entry.isIntersecting) continue;
+          if (entry.target.id === FIRST_ITEM_ID) {
+            this._scrollTop();
+          }
+          if (entry.target.id === LAST_ITEM_ID) {
+            this._loadMore();
+          }
+          this.intersectionObserver.unobserve(entry.target);
+        }
+      });
+    },
+    mounted() {
+    },
+    methods: {
+      setScrollList(scrollList) {
+        scrollList.forEach(panel => {
+          panel.list[0].id = ''
+          panel.list[0].ref = ''
+          panel.list[panel.list.length - 1].id = ''
+          panel.list[panel.list.length - 1].ref = ''
+        })
+
+        const scrollListLen = scrollList.length - 1
+        const len = scrollList[scrollListLen].list.length - 1
+        scrollList[0].list[0].id = FIRST_ITEM_ID
+        scrollList[0].list[0].ref = 'firstItem'
+        scrollList[scrollListLen].list[len].id = LAST_ITEM_ID;
+        scrollList[scrollListLen].list[len].ref = 'lastItem';
+
+        this.scrollList = scrollList
+      },
+      _loadMore() {
+        if (this.list.length > this.page * this.pageSize) {
+          return this._scrollBottom()
+        }
+        this.loading = true
+        this.loadMore().then(more => {
+          this.loading = false
+          this.page++
+          if (more.length < this.pageSize) this.ended = true;
+
+          const list = JSON.parse(JSON.stringify(more));
+
+          if (this.scrollList.length >= 3) {
+            const ref = this.scrollList[0].ref
+            const padding = this.$refs[ref][0].scrollHeight;
+            this.paddingTop += padding
+            const [_, ...other] = this.scrollList
+            this.scrollList = [...other]
+          }
+
+          const scrollList = [...this.scrollList, {
+            page: this.page,
+            ref: 'panel-' + this.page,
+            list
+          }]
+
+          this.setScrollList(scrollList)
+
+          this.list = [...this.list, ...list];
+          this.$nextTick(() => {
+            this.setIntersectionObserver();
+          });
+        });
+      },
+      _scrollBottom() {
+        const [first, second, third] = this.scrollList;
+        const padding = this.$refs[first.ref][0].scrollHeight
+        this.paddingTop += padding;
+
+        const bottomList = this.list.slice(this.page * this.pageSize, (this.page + 1) * this.pageSize)
+
+        this.page++
+        const scrollList = [
+          second,
+          third,
+          {
+            page: this.page,
+            ref: 'panel-' + this.page,
+            list: bottomList
+          }
+        ]
+        this.setScrollList(scrollList)
+
+        this.$nextTick(() => {
+          this.setIntersectionObserver()
+
+          const scrollList = this.scrollList;
+          const ref = scrollList[scrollList.length - 1].ref
+          const padding = this.$refs[ref][0].scrollHeight
+          this.paddingBottom -= padding;
+        })
+      },
+      _scrollTop() {
+        if (this.page <= this.scrollList.length) return
+        const [first, second, third] = this.scrollList;
+
+        const padding = this.$refs[third.ref][0].scrollHeight
+        this.paddingBottom += padding;
+
+        const topList = this.list.slice((this.page - 4) * this.pageSize, (this.page - 3) * this.pageSize)
+
+        const scrollList = [
+          {
+            page: this.page - 3,
+            ref: 'panel-' + (this.page - 3),
+            list: topList
+          },
+          first,
+          second
+        ]
+        this.setScrollList(scrollList)
+        this.page--;
+
+        this.$nextTick(() => {
+          this.setIntersectionObserver()
+
+          const scrollList = this.scrollList
+          const ref = scrollList[0].ref
+          const padding = this.$refs[ref][0].scrollHeight
+          this.paddingTop -= padding;
+        })
+      },
+      setIntersectionObserver() {
+        const first = this.$refs.firstItem
+        if (first) {
+          this.firstItem = first[0]
+          this.intersectionObserver.observe(this.firstItem)
+        }
+        this.lastItem = this.$refs.lastItem[0];
+        this.intersectionObserver.observe(this.lastItem);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .infinite-scroll {
+    width: 100%;
+  }
+</style>
